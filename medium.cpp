9ad103416d243a1c0089e79761f44ef41ba1d45b
0Leviathan3 <levin.grune@unixzen.de>
2025-05-27 14:48:39 +0200

    gerüst ändern auf bloß elternklasse

--- medium.cpp
+++ medium.cpp
@@ -1,9 +1,43 @@
-
 #include <iostream>
-#include "medium.h"
 using namespace std;
+#include "medium.h"
 
-Medium::Medium(string title) {
-    this->title = title;
+int Medium::nextId = 1; // Static member to keep track of the next ID
+
+Medium::Medium(string titel, string autor, string genre) 
+    : title(titel), autor(autor), id(nextId++), genre(genre) {
+    // Constructor implementation
 }
+Medium::~Medium() {
+    // Destructor implementation
+}
+void Medium::displayDetails() {
+    cout << "Title: " << title << ", Author: " << autor 
+         << ", ID: " << id << ", Genre: " << genre << endl;
+}
+string Medium::getTitle() const {
+    return title;
+}
+string Medium::getAutor() const {
+    return autor;
+}
+int Medium::getId() const {
+    return id;
+}
+string Medium::getGenre() const {
+    return genre;
+}
+void Medium::setTitle(const string& newTitle) {
+    title = newTitle;
+}
+void Medium::setAutor(const string& newAutor) {
+    autor = newAutor;
+}
+void Medium::setId(int newId) {
+    id = newId;
+}
+void Medium::setGenre(const string& newGenre) {
+    genre = newGenre;
+}
+
 
--- medium.h
+++ medium.h
@@ -4,13 +4,24 @@
 
 class Medium {
 public:
-    Medium(const std::string title);
-    virtual ~Medium() = default;
-
-    virtual void displayDetails() const = 0;
+    Medium(string titel, string autor, string genre);
+    virtual ~Medium();
+    virtual void displayDetails();
+    string getTitle() const;
+    string getAutor() const;
+    int getId() const;
+    string getGenre() const;
+    void setTitle(const string& newTitle);
+    void setAutor(const string& newAutor);
+    void setId(int newId);
+    void setGenre(const string& newGenre);
 
 protected:
-    std::string title;
+    string title;
+    string autor;
+    int id; // Unique identifier for each medium
+    static int nextId; // Static member to keep track of the next ID
+    string genre;
 };
 
 #endif // MEDIUM_H
